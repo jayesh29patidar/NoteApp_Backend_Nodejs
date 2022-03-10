ec75884e149d734998a43df5fb218af9c598aa3a
jayesh29patidar <61138490+jayesh29patidar@users.noreply.github.com>
2022-03-11 02:11:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -127,48 +127,4 @@ app.post("/api/user/notes/notes-by-user", (req, res) => {
     });
 });
 
-// _________________________________________________________________________
-
-app.post('/api/posts', verifyToken, (req, res) => {
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
-        if (err) {
-            res.sendStatus(403);
-        } else {
-            res.json({
-                message: 'Post created...',
-                authData
-            });
-        }
-    });
-});
-
-app.post('/api/login', (req, res) => {
-    // Mock user
-    const user = {
-        id: 1,
-        username: 'brad',
-        email: 'brad@gmail.com'
-    }
-
-    jwt.sign({ user }, 'secretkey', { expiresIn: '30s' }, (err, token) => {
-        res.json({
-            token
-        });
-    });
-});
-
-// Verify Token
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
-    } else {       
-        res.sendStatus(403);
-    }
-
-}
-
 app.listen(5000, () => console.log('Server started on port 5000'));
